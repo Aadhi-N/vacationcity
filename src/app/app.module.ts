7dc55e885ee5650c46083fe44767ed540ac844af
Fathima-N <aadhi.nizamdeen@gmail.com>
2018-10-02 15:03:10 -0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { InputFormComponent } from './input-form/input-form.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FooterComponent } from './footer/footer.component';
     MessagesComponent,
     InputFormComponent,
     SearchResultsComponent,
-    FooterComponent
+    FooterComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+// import { } from "@types/googlemaps";
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  // @ViewChild('gmap') gmapElement: any;
+  // map: google.maps.Map;
+
+  constructor() { }
+
+  ngOnInit() {
+  //   var mapProp = {
+  //     center: new google.maps.LatLng(18.5793, 73.8143),
+  //     zoom: 15,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+
+  //   this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+
+}
