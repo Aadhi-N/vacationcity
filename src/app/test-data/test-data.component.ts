5b1c5c2b60274b2fd00564cf3b5270dc6b212853
Fathima-N <aadhi.nizamdeen@gmail.com>
2018-10-04 20:34:34 -0700

    testing heroku database on front end

--- src/app/test-data/test-data.component.ts
+++ src/app/test-data/test-data.component.ts
@@ -20,7 +20,7 @@ export class TestDataComponent implements OnInit {
 
   getHerokuData() {
     this.herokuDatabaseService.getHerokuData().subscribe(herokuData => {
-      this.herokuData = herokuData[0];
+      this.herokuData = herokuData;
       console.log('herokuData', herokuData);
     })
   }
